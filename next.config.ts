47c86b5997429b8a7cb4555dc2050a16209bbc61
kiran-k-k <kendrek57@gmail.com>
2026-08-02 15:01:38 +0530

    Disable missingSuspenseWithCSRBailout to fix Vercel build

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,9 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
 export default nextConfig;
